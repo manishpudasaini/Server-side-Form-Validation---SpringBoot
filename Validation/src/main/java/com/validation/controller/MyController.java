82c5842c11687e4b62fd58e5bba78c48935a96ec
manishpudasaini <eziomanish1@gmail.com>
2023-04-17 21:47:41 +0545

    form validation - server side validation

---
+++ Validation/src/main/java/com/validation/controller/MyController.java
@@ -0,0 +1,35 @@
+package com.validation.controller;
+
+import com.validation.entities.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class MyController {
+    @GetMapping("/home")
+    public String homePage(Model model){
+        model.addAttribute("loginData",new User());
+        return "formFragment";
+    }
+
+    @PostMapping("/save")
+    public String submitPage(@ModelAttribute("loginData") User user , Model model){
+        System.out.println(user);
+        model.addAttribute("username" , user.getUsername());
+        model.addAttribute("password",user.getPassword());
+        return "newPage";
+    }
+
+//    @GetMapping("/success")
+//    public String afterLogin(){
+//        return "newPage";
+//    }
+
+
+
+
+
+}
---
+++ Validation/src/main/java/com/validation/entities/User.java
@@ -0,0 +1,14 @@
+package com.validation.entities;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class User {
+
+    private String username;
+    private String password;
+}
