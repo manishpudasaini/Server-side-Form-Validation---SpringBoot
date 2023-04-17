b032d7e84d29298ad2867fd9fd138e7839f17f73
manishpudasaini <eziomanish1@gmail.com>
2023-04-17 22:35:26 +0545

    form validation - server side validation

--- Validation/src/main/java/com/validation/controller/MyController.java
+++ Validation/src/main/java/com/validation/controller/MyController.java
@@ -3,9 +3,12 @@ package com.validation.controller;
 import com.validation.entities.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
 public class MyController {
@@ -16,11 +19,13 @@ public class MyController {
     }
 
     @PostMapping("/save")
-    public String submitPage(@ModelAttribute("loginData") User user , Model model){
-        System.out.println(user);
-        model.addAttribute("username" , user.getUsername());
-        model.addAttribute("password",user.getPassword());
-        return "newPage";
+    public String submitPage(@Valid @ModelAttribute("loginData") User user , Model model){
+            System.out.println(user);
+            model.addAttribute("username" , user.getUsername());
+            model.addAttribute("password",user.getPassword());
+            return "newPage";
+
+
     }
 
 //    @GetMapping("/success")
--- Validation/src/main/java/com/validation/entities/User.java
+++ Validation/src/main/java/com/validation/entities/User.java
@@ -2,13 +2,21 @@ package com.validation.entities;
 
 import lombok.*;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
 public class User {
-
+    @NotBlank(message = "Username cann't be empty!!!")
+    @Size(min = 4,max = 30 , message = "Username should must be between 4 to 30")
     private String username;
+
+
+    @NotBlank(message = "Password cann't be empty!!!")
+    @Size(min = 4,max = 15 , message = "Username should must be between 6 to 15")
     private String password;
 }
